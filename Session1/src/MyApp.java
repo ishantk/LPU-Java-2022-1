e116c26b973e4e443455c5403d9537aa0fabe93a
ishantk <er.ishant@gmail.com>
2022-01-19 08:17:19 +0530

    Session1

---
+++ Session1/src/MyApp.java
@@ -0,0 +1,16 @@
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// Simply Code here in the main
+		System.out.println("We are learning java. stick to the concepts well");
+		System.out.println("This is all for the very 1st session");
+		System.out.println("Thank You. I hope you all enjoyed");
+
+	}
+
+}
+
+class One{
+	
+}
